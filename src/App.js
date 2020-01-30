25c20f68791bbd1f01d893e9f6040f917d5d9cb7
Miss-Cheese <nat.galligan@gmail.com>
2020-01-30 10:00:11 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,28 @@ class App extends React.Component {
 
   state = {
     articles: articles,
+    view: "Card",
+    theme: "light"
+  }
+
+  clickHander = (event) => {
+   // if card view - renderDiv with class name "card" & function renderArticles
+   // if list view - render div with class name "list" & renderList
+    
+   if (this.state.view === "Card") {
+     return (
+       <div className="card">
+            {this.renderArticles()}
+          </div>
+        )
+      } else {
+        return (
+          <div className="list">
+            {this.renderList()}
+          </div>
+        )
+      }
+
   }
 
   renderArticles = () => {
@@ -24,14 +46,36 @@ class App extends React.Component {
     })
   }
 
+  renderList = () => {
+    return this.state.articles.map(article => {
+      return <ArticleItem 
+              key={article.id} 
+              title={article.title}
+              />
+    })
+  }
+
+  helperFunc = (event) => {
+    console.log(event.target.id)
+    this.setState({
+      view: event.target.id
+    })
+  }
+
+  themeSwitch = (event) => {
+    console.log(event.target.parentNode.className)
+    this.setState({
+      theme: event.target.parentNode.className
+    })
+  }
+  
+
   render(){
     return (
-      <div className="light">
-        <button>Switch to Dark Mode</button>
-        <button>Switch to List View</button>
-        <div className="cards">
-          {this.renderArticles()}
-        </div>
+      <div className={this.state.theme === "light" ? "dark" : "light"}>
+        <button onClick={this.themeSwitch}>Switch to {this.state.theme === "light" ? "Light" : "Dark"} Mode</button>
+        <button id={this.state.view === 'Card' ? "List" : "Card"} onClick={this.helperFunc}>Switch to {this.state.view === 'Card' ? "List" : "Card"} View</button>
+        {this.clickHander()}
       </div>
     );
   }
